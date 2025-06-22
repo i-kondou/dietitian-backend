d155e39f7200b21bf0781ef5e51adb5489a92da8
i-kondou <u573077g@ecs.osaka-u.ac.jp>
2025-06-22 06:59:29 +0000

    firebase機能の追加

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,15 +1,14 @@
 from src.routers import dummy
 from fastapi import FastAPI
-# import firebase_admin
-# from dotenv import load_dotenv
-# from firebase_admin.credentials import Certificate
+import firebase_admin
+from firebase_admin.credentials import Certificate
 
-# load_dotenv()
+cred_path = "/secrets/firebase-adminsdk.json"
 
 
 app = FastAPI()
 
 app.include_router(dummy.router, prefix="/dummy", tags=["dummy"])
 
-# cred = Certificate("path/to/your/serviceAccountKey.json")
-# firebase_admin.initialize_app(cred)
+cred = Certificate(cred_path)
+firebase_admin.initialize_app(cred)
